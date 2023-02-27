c6aa2fca494dee99f2a7b0c81585d19dfacf5f32
Petrov Andrey <andreiptrz@yandex.ru>
2023-02-27 22:01:55 +0200

    fix: fixed funktion

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let obj = {};
+
+  bracketsConfig.forEach(element => {
+    obj[element[1]] = element[0];
+  });
+
+  let brackets = [];
+  for (i = 0; i < str.length; i++) {
+    if (str[i] in obj) {
+      if (brackets.length > 0 && obj[str[i]] === brackets[brackets.length - 1]) {
+        brackets.pop();
+      } else {
+        brackets.push(str[i]);
+      };
+    } else {
+      brackets.push(str[i]);
+    };
+  };
+  if (brackets.length === 0) {
+    return true;
+  } else {
+    return false;
+  };
 }
